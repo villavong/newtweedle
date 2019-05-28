21a6246c33330a09db654355365d1081521b36bd
villavong <asyao@naver.com>
2019-05-27 19:23:16 -0700

    PROFILE_Error

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,7 +14,7 @@ export const GET_PROFILE = 'GET_PROFILE';
 export const GET_PROFILES = 'GET_PROFILES';
 //
 
-export const PROFILE_ERROR = 'PROFIL_ERROR';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
