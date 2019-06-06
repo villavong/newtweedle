ac364957cf8c51df38cf25d7fac75f7d2d17249f
villavong <asyao@naver.com>
2019-06-06 09:06:03 -0700

    experience

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -26,8 +26,8 @@ const AddExperience = ({ addExperience, history }) => {
     <Fragment>
       <h1 className='large text-primary'>Add An Experience</h1>
       <p className='lead'>
-        <i className='fas fa-code-branch' /> Add any developer/programming
-        positions that you have had in the past
+        <i className='fas fa-code-branch' /> Add any positions that you have had
+        in the past
       </p>
       <small>* = required field</small>
       <form
